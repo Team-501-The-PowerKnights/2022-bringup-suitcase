8607590f24730a932a42a8108e8e165cc67b48c8
Java4First <47263257+Java4First@users.noreply.github.com>
2022-01-30 11:47:19 -0500

    Add HW Vendor Libs (#8)
    Put all the HW vendor libraries into the controlled configuration.

--- src/main/java/frc/robot/config/BuildVersionInfo.java
+++ src/main/java/frc/robot/config/BuildVersionInfo.java
@@ -19,6 +19,6 @@ package frc.robot.config;
  **/
 class BuildVersionInfo {
     public static final String programmer = "Stu Lewin";
-    public static final String commitSHA = "dc63dc2";
-    public static final String timeStamp = "20220121-195720";
+    public static final String commitSHA = "b977a6c";
+    public static final String timeStamp = "20220123-141515";
 }
